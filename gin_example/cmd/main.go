cd09cdfaffc27eda9e94cf819153cf83e3df6be6
Developer Writings <developerwritings@gmail.com>
2023-05-13 23:22:00 +0530

    Initial Commit Using Gin Example

--- gin_example/cmd/main.go
+++ gin_example/cmd/main.go
@@ -1,5 +1,17 @@
-package main 
+package main
 
-func main () {
-	fmt.Println("Gin Example")
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
