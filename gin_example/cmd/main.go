74434ac4a434a5582e37fe99255ad98c60120cb4
Developer Writings <developerwritins@gmail.com>
2023-05-13 22:47:44 -0700

    Add Routes

--- gin_example/cmd/main.go
+++ gin_example/cmd/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+// album represents data about a record album.
+type user struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"artist"`
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
-	})
-	r.Run()
+	router := gin.Default()
+	router.GET("/", Homepage)
+	router.GET("/user/:id", getUserByID)
+	router.POST("/user/create", CreateUser)
+
+	router.Run("localhost:8080")
+}
+
+func Homepage(c *gin.Context) {
+}
+
+func CreateUser(c *gin.Context) {
+	var newUser user
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+
+}
+
+func getUserByID(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Println(id)
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User Not Found"})
 }
