bf52ec2a2275b8b9f3e8818ebf6a730e43681e63
Developer Writings <developerwritins@gmail.com>
2023-05-13 23:43:11 -0700

    Updated code to folder structure

--- gin_example/cmd/main.go
+++ gin_example/cmd/main.go
@@ -2,42 +2,19 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
+	"githhub.com/developerwritings/go_development/gin_example/config"
+	"githhub.com/developerwritings/go_development/gin_example/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type user struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"artist"`
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/", Homepage)
-	router.GET("/user/:id", getUserByID)
-	router.POST("/user/create", CreateUser)
-
-	router.Run("localhost:8080")
-}
-
-func Homepage(c *gin.Context) {
-}
-
-func CreateUser(c *gin.Context) {
-	var newUser user
-
-	if err := c.BindJSON(&newUser); err != nil {
-		return
-	}
-
-}
-
-func getUserByID(c *gin.Context) {
-	id := c.Param("id")
-	fmt.Println(id)
-
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User Not Found"})
+	router.GET("/user/:id", handlers.GetUserByID)
+	router.POST("/user/create", handlers.CreateUser)
+	serverConfig := config.Config{}
+	serverConfig.NewConfig()
+	config := serverConfig.ServerConfig()
+	fmt.Println(serverConfig.Port)
+	router.Run(":" + config.Port)
 }
